e9b8f525d088cf3a334f070897ba7286369c2dc0
Rasel141 <rrasel141@gmail.com>
2019-01-15 13:35:02 +0600

    Add react router setup & dummy component

--- src/app.js
+++ src/app.js
@@ -1,6 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter,  Route} from 'react-router-dom'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+    <div>
+        This is from my dashboard component
+    </div>
+)
+
+const AddExpensePage = () => (
+    <div>
+        This is add expense page
+    </div>
+)
+
+const EditExpensePage = () => (
+    <div>
+        This is edit expense page
+    </div>
+)
+
+const HelpPage = () => (
+    <div>
+        This is help expense page
+    </div>
+)
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" exact component={ExpenseDashboardPage} />
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+        </div>
+    </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('app'));
